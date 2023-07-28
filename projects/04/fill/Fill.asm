be8560f4e4b278320601482ce10caad243d97d4f
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-07-28 22:43:48 +0800

    write Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    @KBD
+    D=M
+    @DRAW_WHITE
+    D;JEQ       // If RAM[KBD] == 0, goto DRAW_WHITE
+    @DRAW_BLACK
+    0;JMP       // Else, goto DRAW_BLACK
+
+(DRAW_WHITE)
+    @i
+    M=0         // i = 0
+
+(LOOP_WHITE)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGE       // If i >= 8192, goto LOOP
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0         // Set to white
+
+    @i
+    M=M+1       // i = i + 1
+    @LOOP_WHITE
+    0;JMP
+
+(DRAW_BLACK)
+    @i
+    M=0         // i = 0
+
+(LOOP_BLACK)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGE       // If i >= 8192, goto LOOP
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1        // Set to black
+
+    @i
+    M=M+1       // i = i + 1
+    @LOOP_BLACK
+    0;JMP
