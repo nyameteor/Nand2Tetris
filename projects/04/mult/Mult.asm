55cc97a53230fd17e9acb42fee3cf7f55635f7d0
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-07-28 20:45:56 +0800

    write Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,36 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @res
+    M=0     // res = 0
+
+    @i
+    M=0     // i = 0
+
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M
+    @STOP
+    D;JGE   // If (i-R1) >= 0 goto STOP
+
+    @R0
+    D=M
+    @res
+    M=M+D   // res = res + R0
+
+    @i
+    M=M+1   // i = i + 1
+    @LOOP
+    0;JMP   // Goto LOOP
+
+(STOP)
+    @res
+    D=M
+    @R2
+    M=D     // R2 = res
+
+(END)
+    @END
+    0;JMP   // Infinite loop
